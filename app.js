5ffa5fddefc43b520bfcfabbe5d20b14039fe65f
Syunsuke Shima <s.syun617@icloud.com>
2023-06-28 10:25:59 +0900

    ✨ 編集のフォームとルーティングの作成

--- app.js
+++ app.js
@@ -44,6 +44,11 @@ app.get("/campgrounds/:id", async (req, res) => {
   res.render("campgrounds/show", { campground });
 });
 
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
 app.listen(3000, (req, res) => {
   console.log("ポート3000で待受中...");
 });
