65676fb1451ce03b9a35132c52bfe6a7239c1ffe
Syunsuke Shima <s.syun617@icloud.com>
2023-09-18 23:59:19 +0900

    ✨ passportをexpressで使えるようセットアップ

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressEroor");
 const methodOverride = require("method-override");
-const campgroundRoutes = require("./routes/campgrounds");
-const reviewRoutes = require("./routes/reviews");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp", {
@@ -43,6 +47,13 @@ const sessionConfig = {
   },
 };
 app.use(session(sessionConfig));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(flash());
 
 app.use((req, res, next) => {
