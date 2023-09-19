2572a78c7fdcf812d50b46b15da464fa6d362446
Syunsuke Shima <s.syun617@icloud.com>
2023-09-19 15:30:40 +0900

    ✨ ユーザー登録用のフォームを追加

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const user = require("../models/user");
+const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
 router.post("/register", async (req, res) => {
-  res.send(req.body);
+  try {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registerdUser = await User.register(user, password);
+    console.log(registerdUser);
+    req.flash("success", "Yelp Campへようこそ");
+    res.redirect("/campgrounds");
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/register");
+  }
 });
 
 module.exports = router;
