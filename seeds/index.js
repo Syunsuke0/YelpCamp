3d2e8ee8bc30162ff0bf6638b67c386e6dd35228
Syunsuke Shima <s.syun617@icloud.com>
2023-09-21 12:46:26 +0900

    ✨ 誰が登録したキャンプ場を表示

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
     const randomCityIndex = Math.floor(Math.random() * cities.length);
     const price = Math.floor(Math.random() * 2000) + 1000;
     const camp = new Campground({
+      author: "650902149a3ca48f055fc740",
       location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
       title: `${sample(descriptors)}・${sample(places)}`,
       image:
