cfc9f4da4d04f70dc5282159d8719b9809985fe9
Syunsuke Shima <s.syun617@icloud.com>
2023-09-27 00:52:36 +0900

    ♻️ review,user関連のコントローラの追加

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const users = require("../controllers/users");
 const passport = require("passport");
 const User = require("../models/user");
 
-router.get("/register", (req, res) => {
-  res.render("users/register");
-});
+router.get("/register", users.renderRegister);
 
-router.post("/register", async (req, res) => {
-  try {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    const registerdUser = await User.register(user, password);
-    req.login(registerdUser, (err) => {
-      if (err) return next(err);
-      req.flash("success", "Yelp Campへようこそ");
-      res.redirect("/campgrounds");
-    });
-  } catch (e) {
-    req.flash("error", e.message);
-    res.redirect("/register");
-  }
-});
+router.post("/register", users.register);
 
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
+router.get("/login", users.renderLogin);
 
 router.post(
   "/login",
@@ -34,21 +17,9 @@ router.post(
     failureRedirect: "/login",
     keepSessionInfo: true,
   }),
-  (req, res) => {
-    req.flash("success", "おかえりなさい!!");
-    const redirectUrl = req.session.returnTo || "/campgrounds";
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-  }
+  users.login
 );
 
-router.get("/logout", (req, res) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success", "ログアウトしました。");
-    res.redirect("/campgrounds");
-  });
-});
+router.get("/logout", users.logout);
+
 module.exports = router;
