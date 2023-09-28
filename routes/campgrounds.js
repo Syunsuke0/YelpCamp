457e19c56cd2f28cf1785d97c3cdc06b5a728163
Syunsuke Shima <s.syun617@icloud.com>
2023-09-29 08:34:44 +0900

    ✨ 画像をcloudinaryに保存できるように設定

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
