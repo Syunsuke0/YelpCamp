e1bb68fa65c3b7279456e7776e7532af000cd0e3
Syunsuke Shima <s.syun617@icloud.com>
2023-10-06 00:30:13 +0900

    🚧 キャンプ場を地図の中心に表示

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11",
-  center: [139.7671, 35.6812],
-  zoom: 6,
+  center: campground.geometry.coordinates,
+  zoom: 8,
 });
+
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
