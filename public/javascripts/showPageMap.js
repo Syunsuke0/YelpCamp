686a96ecf53a47da16f09ec782624981d8d9d2e4
Syunsuke Shima <s.syun617@icloud.com>
2023-10-08 00:10:16 +0900

    ✨ 💄ポップアップの追加とマップのスタイル変更

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,9 +1,16 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
+  style: "mapbox://styles/mapbox/light-v10",
   center: campground.geometry.coordinates,
   zoom: 8,
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .addTo(map)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${campground.title}</h4><p>${campground.location}</p>`
+    )
+  );
